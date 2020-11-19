7d922b73965b5682f570f2fac914b583dd15af59
Bart Massey <bart.massey@gmail.com>
2020-11-19 01:03:59 -0800

    added more tests, negative tests

--- tests.cpp
+++ tests.cpp
@@ -5,32 +5,69 @@ TEST(IsPalindromeTest, racecar) {
     ASSERT_EQ(true, isPalindrome("racecar"));
 }
 
+
+TEST(IsPalindromeTest, racecap) {
+    ASSERT_EQ(false, isPalindrome("racecap"));
+}
+
+TEST(IsPalindromeTest, pacecar) {
+    ASSERT_EQ(false, isPalindrome("pacecar"));
+}
+
+TEST(IsPalindromeTest, empty) {
+    ASSERT_EQ(true, isPalindrome(""));
+}
+
 TEST(IsPalindromeTest, a) {
     ASSERT_EQ(true, isPalindrome("a"));
+}
+
+TEST(IsPalindromeTest, aa) {
+    ASSERT_EQ(true, isPalindrome("aa"));
+}
+
+TEST(IsPalindromeTest, ab) {
+    ASSERT_EQ(false, isPalindrome("ab"));
 }
 
 TEST(IsPalindromeTest, aba) {
     ASSERT_EQ(true, isPalindrome("aba"));
 }
 
-TEST(IsPalindromeTest, aaaa) {
-    ASSERT_EQ(true, isPalindrome("aaaa"));
+TEST(IsPalindromeTest, abb) {
+    ASSERT_EQ(false, isPalindrome("abb"));
 }
 
-TEST(IsPalindromeTest, racecaa) {
-    ASSERT_EQ(false, isPalindrome("racecaa"));
+TEST(IsPalindromeTest, bba) {
+    ASSERT_EQ(false, isPalindrome("bba"));
 }
 
-TEST(IsPalindromeTest, adasha) {
-    ASSERT_EQ(true, isPalindrome("a-a"));
+TEST(IsPalindromeTest, abba) {
+    ASSERT_EQ(true, isPalindrome("abba"));
+}
+
+TEST(IsPalindromeTest, abab) {
+    ASSERT_EQ(false, isPalindrome("abab"));
+}
+
+TEST(IsPalindromeTest, a_a) {
+    ASSERT_EQ(true, isPalindrome("a_a"));
+}
+
+TEST(IsPalindromeTest, __a_a_) {
+    ASSERT_EQ(true, isPalindrome("__a_a_"));
+}
+
+TEST(IsPalindromeTest, _a_a__) {
+    ASSERT_EQ(true, isPalindrome("_a_a__"));
+}
+
+TEST(IsPalindromeTest, _a_b__) {
+    ASSERT_EQ(false, isPalindrome("_a_b__"));
 }
 
 TEST(IsPalindromeTest, newline) {
     ASSERT_EQ(true, isPalindrome("\n   "));
-}
-
-TEST(IsPalindromeTest, empty) {
-    ASSERT_EQ(true, isPalindrome(""));
 }
 
 TEST(IsPalindromeTest, panama) {
